e101893e0daee321a36926a2232b79eb2109b2b7
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2024-07-10 17:50:48 -0400

    Update WeatherSnoop.js

--- WeatherSnoop.js
+++ WeatherSnoop.js
@@ -96,7 +96,7 @@ async function getLocal() {
 	}
 	obsStations.sort(function (a, b) {
 	return a.distance - b.distance
-	console.log(obsStations);
+	console.log(obsStations)};
 }
 
 async function loadXMLDoc() {

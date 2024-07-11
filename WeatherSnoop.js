b612fb01c61533af7817c13c9123e7f22b14684c
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2024-07-10 20:07:14 -0400

    Update WeatherSnoop.js

--- WeatherSnoop.js
+++ WeatherSnoop.js
@@ -67,7 +67,7 @@ async function logUse(lng,lat) {
 	/*var geoResp = await getGeo.json(); let address = geoResp.features[0].place_name*/
 	var response = await fetch('https://api.13media13.com/weathersnooper/access/' + ip.ip)
 	var logUpdate = await response.json();
-	console.log(response);
+	console.log(logUpdate);
 	//var response = await fetch('http://localhost:5000/weathersnooper/access/' + address)		
 }
 

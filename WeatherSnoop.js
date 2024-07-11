80c75cb6e03de8be0446837022155e73c8bc990e
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2024-07-10 20:03:42 -0400

    Update WeatherSnoop.js

--- WeatherSnoop.js
+++ WeatherSnoop.js
@@ -62,7 +62,7 @@ function loadMap() {
 async function logUse(lng,lat) {
 	let getIP = await fetch("https://api.ipify.org/?format=json");
 	let ip = await getIP.json();
-	console.log(ip);
+	console.log(ip.ip);
 	var getGeo = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/' + lng + ',' + lat + '.json?types=address&limit=1&access_token=' + mapboxgl.accessToken +''); 
 	var geoResp = await getGeo.json(); let address = geoResp.features[0].place_name
 	var response = await fetch('https://api.13media13.com/weathersnooper/access/' + ip)

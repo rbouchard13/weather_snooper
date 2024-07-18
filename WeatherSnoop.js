b0696e63dddd4d4ea228093dd9a40696948bbb6f
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2024-07-18 18:36:36 -0400

    Update WeatherSnoop.js

--- WeatherSnoop.js
+++ WeatherSnoop.js
@@ -58,7 +58,7 @@ function loadMap() {
 
 function addMarkers(lng,lat){
 	var getObs = fetch('./data.json');
-	var Obs = .JSON.parse(getObs);
+	var Obs = JSON.parse(getObs);
 	for (let i = 0; i < Obs.length; i++) {
 		let lat2 = Obs[i].latitude
 		let lng2 = "" + Obs[i].longitude + "";

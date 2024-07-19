ec0d8d784a2e6f655e5ed2f04a5240cc21cdc92c
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2024-07-19 09:03:55 -0400

    Update WeatherSnoop.js

--- WeatherSnoop.js
+++ WeatherSnoop.js
@@ -35,22 +35,6 @@ geocoder.on('clear', () => {
 });
 
 async function loadMap() {
-	/*var Obs = async () => {
-		var getObs = await fetch('./data.json');
-	}
-	console.log(Obs);*/
-	var getObs = fetch('https://weathersnooper.com/data.json');console.log(getObs);
-	var Obs = await getObs.json();
-	for (let i = 0; i < Obs.length; i++) {
-		let lat2 = Obs[i].latitude
-		let lng2 = "" + Obs[i].longitude + "";
-		let dist = distance(lat, lat2, lng, lng2);
-		let data = {name: Obs[i].icao, lat: lat2, lng: lng2, distance: dist, airport: Obs[i].airport};
-		obsStations.push(data);
-	}
-	while (obsStations.length = 0) {
-		console.log("gathering data");
-	}
 	var element = document.getElementById('map');
 	map = new mapboxgl.Map({
   		container: 'map',
@@ -65,11 +49,24 @@ async function loadMap() {
 			lng = pos.coords.longitude;
 			logUse(lng,lat);
 			addMarkers(lng,lat);
-			loadXMLDoc();
+			//loadXMLDoc();
 		})
 	} else {
 			alert("Geolocation is not supported by this browser.");
 	}	
+}
+
+obsStations = async() => {
+	var getObs = fetch('https://weathersnooper.com/data.json');console.log(getObs);
+	var Obs = await getObs.json();
+	for (let i = 0; i < Obs.length; i++) {
+		let lat2 = Obs[i].latitude
+		let lng2 = "" + Obs[i].longitude + "";
+		let dist = distance(lat, lat2, lng, lng2);
+		let data = {name: Obs[i].icao, lat: lat2, lng: lng2, distance: dist, airport: Obs[i].airport};
+		obsStations.push(data);
+	}
+	loadXMLDoc();
 }
 
 function addMarkers(lng,lat){
